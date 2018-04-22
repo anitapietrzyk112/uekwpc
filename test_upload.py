7d3f9c4fc111874d477f65648ca491f9c9d676d3
EC2 Default User <ec2-user@ip-172-31-31-45.eu-central-1.compute.internal>
2018-04-22 12:46:48 +0000

    dodane

---
+++ test_upload.py
@@ -0,0 +1,14 @@
+import boto3
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('204187-apietrzy')
+
+my_file = open('to_be_uploaded.txt', 'w+')
+my_file.write('Here will be my test content, isnt it cool?')
+my_file.close()
+
+bucket.put_object(
+ Key='omega/gamma/test.txt',
+ Body=open('to_be_uploaded.txt', 'rb')
+)
+
